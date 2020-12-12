2799083f7eb6684b3dc4a9964bec87518d17a415
Mehdi Khodadadi <mehdikhodadadi@outlook.com>
2020-12-12 21:34:06 +0330

    Fixing some bugs

--- projects/test-jalali-date-picker/src/app/app.module.ts
+++ projects/test-jalali-date-picker/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NzDatePickerModule } from '../../../../dist/ng-zorro-jalali-date-picker/';
-//import { NzDatePickerModule } from '../../../ng-zorro-jalali-date-picker/src/';
+//import { NzDatePickerModule } from '../../../../dist/ng-zorro-jalali-date-picker/';
+import { NzDatePickerModule } from '../../../ng-zorro-jalali-date-picker/src/';
 
 @NgModule({
   declarations: [
